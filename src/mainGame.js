d95f3961802fb47b210c5d27ffa9659688136470
pbaskovi <pbaskovi@gmail.com>
2018-10-06 20:16:51 +0200

    adding bombs

--- src/mainGame.js
+++ src/mainGame.js
@@ -93,6 +93,13 @@ function create() {
   player.body.setGravityY(300);
 
   this.physics.add.collider(player, platforms);
+
+    bombs = this.physics.add.group();
+
+    this.physics.add.collider(bombs, platforms);
+
+    this.physics.add.collider(player, bombs, hitBomb, null, this);
+
 }
 
 function update() {
@@ -112,7 +119,37 @@ function update() {
   }
 }
 function collectStar(player, star) {
-  star.disableBody(true, true);
-  score += 10;
-  scoreText.setText('Score: ' + score);
+    star.disableBody(true, true);
+
+    score += 10;
+    scoreText.setText('Score: ' + score);
+
+    if (stars.countActive(true) === 0)
+    {
+        stars.children.iterate(function (child) {
+
+            child.enableBody(true, child.x, 0, true, true);
+
+        });
+
+        var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+
+        var bomb = bombs.create(x, 16, 'bomb');
+        bomb.setBounce(1);
+        bomb.setCollideWorldBounds(true);
+        bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+        bomb.allowGravity = false;
+
+    }
+}
+
+function hitBomb (player, bomb)
+{
+    this.physics.pause();
+
+    player.setTint(0xff0000);
+
+    player.anims.play('turn');
+
+    gameOver = true;
 }
